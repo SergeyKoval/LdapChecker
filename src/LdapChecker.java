490e7ddf0281181d649db1a11759d3054b81b704
Sergey Koval <skoval@exadel.com>
2015-10-22 11:17:58 +0300

    Add ability to retrieve root DSE

--- src/LdapChecker.java
+++ src/LdapChecker.java
@@ -9,7 +9,6 @@ import java.net.InetAddress;
 import java.util.*;
 
 public class LdapChecker {
-
     private static final String INITIAL_CONTEXT_FACTORY_IMPLEMENTATION = "com.sun.jndi.dns.DnsContextFactory";
     private static final String DNS = "dns:";
     private static final String LDAP_TAP_PREFIX = "_ldap._tcp.";
@@ -23,13 +22,13 @@ public class LdapChecker {
     private static final String CONFIGURATION_FILE_NAME = "common.properties";
     private static final String EQUALS = "=";
     private static final String LDAP_USER_BASE = "ldap.user.base";
+    private static final String LDAP_AUTODISCOVER_BASE = "ldap.autodiscover.base";
     private static final String LDAP_USER_QUERY = "ldap.user.query";
     private static final String LDAP_USER_ATTRIBUTES = "ldap.user.attributes";
     private static final String COMMA = ",";
     private static final String LDAP_GLOBAL_URL_ATTRIBUTE = "ldap.global.url";
     private static final String LDAP_AUTODISCOVER_FILTER = "(&(objectClass=serviceConnectionPoint)(|(keywords=67661d7F-8FC4-4fa7-BFAC-E1D7794C1F68)(keywords=77378F46-2C66-4aa9-A6A6-3E7A48B19596)))";
     private static final String LDAP_AUTODISCOVER_ATTRIBUTE = "ldap.autodiscover.attribute";
-    private static final String CONFIGURATION_BASE = "cn=Configuration";
 
     private String LDAP_BASE;
     private String LDAP_QUERY;
@@ -37,12 +36,12 @@ public class LdapChecker {
     private String LDAP_GLOBAL_URL;
     private String[] LDAP_AUTODISCOVER_ATTRIBUTE_VALUE;
     private PrintWriter fileWriter;
+    private String AUTODISCOVER_BASE;
 
     public static void main (String[] args) {
-        List<String> servers;
-        String login = "";
-        String password = "";
-        String domain = "";
+        String login;
+        String password;
+        String domain;
 
         try {
             BufferedReader bread = new BufferedReader(new InputStreamReader(System.in));
@@ -63,65 +62,8 @@ public class LdapChecker {
             System.out.println("Reading ldap parameters from the properties file...");
             main.readLdapParameters();
             System.out.println("Successfully read");
-            if (main.LDAP_BASE == null || main.LDAP_BASE.length() == 0) {
-                System.out.println("Parse ldap.base from domain...");
-                String[] domainItems = domain.split("\\.");
-                StringBuilder ldapBaseBuilder = new StringBuilder();
-                for (String domainItem : domainItems) {
-                    ldapBaseBuilder.append("dc=");
-                    ldapBaseBuilder.append(domainItem);
-                    ldapBaseBuilder.append(",");
-                }
-                if (ldapBaseBuilder.length() > 0) {
-                    ldapBaseBuilder.deleteCharAt(ldapBaseBuilder.length() - 1);
-                }
-                main.LDAP_BASE = ldapBaseBuilder.toString();
-                System.out.println("ldap.base=" + main.LDAP_BASE);
-                main.fileWriter.println("ldap.base=" + main.LDAP_BASE);
-            }
-            main.fileWriter.println("-------Init Ldap check for user '" + login + "' with domain '" + domain + "'-------");
-            System.out.println("-------Init Ldap check for user '" + login + "' with domain '" + domain + "'-------");
 
-            if (main.useGlobalUrl()) {
-                System.out.println("Check direct Ldap Global Url...");
-                main.fileWriter.println("Direct Ldap server is specified: " + main.LDAP_GLOBAL_URL);
-                System.out.println("Direct Ldap server is specified: " + main.LDAP_GLOBAL_URL);
-                servers = new ArrayList<String>();
-                servers.add(main.LDAP_GLOBAL_URL);
-            } else {
-                System.out.println("Getting top servers...");
-                servers = main.selectTopServers(domain);
-                if (servers == null || servers.isEmpty()) {
-                    System.out.println("No servers found");
-                    return;
-                }
-                System.out.println("Servers received success");
-            }
-
-            System.out.println("Getting ldap attributes...");
-            for (String ldapUrl : servers) {
-                Map<String, List<String>> userAttributes = main.getUserAttributes(login, password, domain, ldapUrl);
-                if (userAttributes != null) {
-                    StringBuilder result = new StringBuilder();
-                    result.append("From ldap '");
-                    result.append(ldapUrl);
-                    result.append("' next attributes were received:");
-                    for (String userAttribute : userAttributes.keySet()) {
-                        result.append("\n\t");
-                        result.append(userAttribute);
-                        result.append("=");
-                        for (String element : userAttributes.get(userAttribute)) {
-                            result.append("\n\t\t");
-                            result.append(element);
-                        }
-                    }
-
-                    main.fileWriter.println(result);
-                    System.out.println(result);
-                }
-            }
-            System.out.println("Finish receiving ldap attributes");
-            main.fileWriter.close();
+            main.checkLdap(domain, login, password);
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (UnsupportedEncodingException e) {
@@ -131,9 +73,92 @@ public class LdapChecker {
         }
     }
 
+    private void checkLdap(String domain, String login, String password) {
+        List<String> servers;
+        if (LDAP_BASE == null || LDAP_BASE.length() == 0) {
+            System.out.println("Parse ldap.base from domain...");
+            String[] domainItems = domain.split("\\.");
+            StringBuilder ldapBaseBuilder = new StringBuilder();
+            for (String domainItem : domainItems) {
+                ldapBaseBuilder.append("dc=");
+                ldapBaseBuilder.append(domainItem);
+                ldapBaseBuilder.append(",");
+            }
+            if (ldapBaseBuilder.length() > 0) {
+                ldapBaseBuilder.deleteCharAt(ldapBaseBuilder.length() - 1);
+            }
+            LDAP_BASE = ldapBaseBuilder.toString();
+            addMessage("ldap.base=" + LDAP_BASE);
+        }
+        addMessage("-------Init Ldap check for user '" + login + "' with domain '" + domain + "'-------");
+
+        if (useGlobalUrl()) {
+            System.out.println("Check direct Ldap Global Url...");
+            addMessage("Direct Ldap server is specified: " + LDAP_GLOBAL_URL);
+
+            servers = new ArrayList<String>();
+            servers.add(LDAP_GLOBAL_URL);
+        } else {
+            System.out.println("Getting top servers...");
+            servers = selectTopServers(domain);
+            if (servers == null || servers.isEmpty()) {
+                System.out.println("No servers found");
+                return;
+            }
+            System.out.println("Servers received success");
+        }
+
+        System.out.println("Getting ldap attributes...");
+        Properties authorizationProperties = prepareAuthorizationProperties(login, domain, password);
+        for (String ldapUrl : servers) {
+            addMessage("----Getting root DSE attributes--------");
+            Map<String, List<String>> rootDSEAttributes = getRootDSEAttributes(ldapUrl, authorizationProperties);
+            addMessageAttributes(rootDSEAttributes, ldapUrl);
+
+            if (LDAP_SEARCH_ATTRIBUTES != null) {
+                addMessage("----Getting autodiscover URL--------");
+                if (AUTODISCOVER_BASE != null) {
+                    addMessage("Use ldap.autodiscover.base from the configuration file: " + AUTODISCOVER_BASE);
+                } else {
+                    AUTODISCOVER_BASE = rootDSEAttributes.get("configurationNamingContext").get(0);
+                    addMessage("Use ldap.autodiscover.base from the LDAP root DSE: " + AUTODISCOVER_BASE);
+                }
+
+                Map<String, List<String>> autodiscoverAttributes = getUserAttributesFromLdap(LDAP_AUTODISCOVER_FILTER, ldapUrl, authorizationProperties, LDAP_AUTODISCOVER_ATTRIBUTE_VALUE, AUTODISCOVER_BASE);
+                addMessageAttributes(autodiscoverAttributes, ldapUrl);
+            }
+
+            addMessage("----Getting user attributes--------");
+            String searchFilter = String.format(LDAP_QUERY, login);
+            Map<String, List<String>> userAttributes = getUserAttributesFromLdap(searchFilter, ldapUrl, authorizationProperties, LDAP_SEARCH_ATTRIBUTES, LDAP_BASE);
+            addMessageAttributes(userAttributes, ldapUrl);
+        }
+        addMessage("Finish receiving ldap attributes");
+        fileWriter.close();
+    }
+
+    private void addMessageAttributes(Map<String, List<String>> attributes, String ldapUrl) {
+        if (attributes != null && !attributes.isEmpty()) {
+            StringBuilder result = new StringBuilder();
+            result.append("From ldap '");
+            result.append(ldapUrl);
+            result.append("' next attributes were received:");
+            for (String attribute : attributes.keySet()) {
+                result.append("\n\t");
+                result.append(attribute);
+                result.append("=");
+                for (String element : attributes.get(attribute)) {
+                    result.append("\n\t\t");
+                    result.append(element);
+                }
+            }
+
+            addMessage(result.toString());
+        }
+    }
 
     private List<String> getADServers(String domain) {
-        List<String> serverList = null;
+        List<String> serverList = new ArrayList<String>();
         DirContext ctx = null;
         try {
             Hashtable<String, String> env = new Hashtable<String, String>();
@@ -144,11 +169,10 @@ public class LdapChecker {
             Attributes attributes = ctx.getAttributes(LDAP_TAP_PREFIX + domain, new String[]{SRV});
             Attribute a = attributes.get(SRV);
 
-            serverList = new ArrayList<String>();
             for (NamingEnumeration n = a.getAll(); n.hasMore(); ) {
                 String serverString = (String) n.next();
-                fileWriter.println("Receive for domain '" + domain + "' server string '" + serverString + "'");
-                System.out.println("Receive for domain '" + domain + "' server string '" + serverString + "'");
+                addMessage("Receive for domain '" + domain + "' server string '" + serverString + "'");
+
                 String[] tokens = serverString.split("\\s+");
                 String server = tokens[3];
                 if (server.endsWith(".")) {
@@ -157,7 +181,7 @@ public class LdapChecker {
                 serverList.add(server);
             }
         } catch (NamingException e) {
-            return null;
+            addMessage("Error on working with ADServers. " + e.getMessage());
         } finally {
             try {
                 if (ctx != null) {
@@ -178,34 +202,54 @@ public class LdapChecker {
             try {
                 if (InetAddress.getByName(serverListItem).isReachable(REACHABLE_TIMEOUT)) {
                     long duration = System.currentTimeMillis() - start;
-                    fileWriter.println("Server '" + serverListItem + "' in domain '" + domain + "' ping=" + duration);
-                    System.out.println("Server '" + serverListItem + "' in domain '" + domain + "' ping=" + duration);
+                    addMessage("Server '" + serverListItem + "' in domain '" + domain + "' ping=" + duration);
                     retList.add(LDAP + serverListItem + SLASH);
                 }
             } catch (IOException e) {
-                fileWriter.println("Server '" + serverListItem + "' in domain '" + domain + "' was not able to ping in " + REACHABLE_TIMEOUT);
-                System.out.println("Server '" + serverListItem + "' in domain '" + domain + "' was not able to ping in " + REACHABLE_TIMEOUT);
+                addMessage("Server '" + serverListItem + "' in domain '" + domain + "' was not able to ping in " + REACHABLE_TIMEOUT);
             }
         }
 
         return retList;
     }
 
-    public Map<String, List<String>> getUserAttributes(String login, String password, String domain, String ldapUrl) {
-        Properties authorizationProperties = prepareAuthorizationProperties(login, domain, password);
-        String searchFilter = String.format(LDAP_QUERY, login);
-        Map<String, List<String>> attrMap = getUserAttributesFromLdap(searchFilter, ldapUrl, authorizationProperties, LDAP_SEARCH_ATTRIBUTES, LDAP_BASE);
+    private Map<String, List<String>> getRootDSEAttributes(String ldapUrl, Properties authorizationProperties) {
+        Map<String, List<String>> attrMap = new HashMap<String, List<String>>();
+        try {
+            DirContext context = LdapCtxFactory.getLdapCtxInstance(ldapUrl, authorizationProperties);
+            SearchControls controls = new SearchControls();
+            controls.setSearchScope(SearchControls.OBJECT_SCOPE);
+            NamingEnumeration answer = context.search("", "(objectclass=*)", controls);
+            if (answer.hasMore()) {
+                SearchResult sr = (SearchResult) answer.next();
+                Attributes attributes = sr.getAttributes();
+                addMessage("Root DSE has " + attributes.size() + " attributes in LDAP");
 
-        if (LDAP_SEARCH_ATTRIBUTES != null) {
-            Map<String, List<String>> autodiscoverAttrMap = getUserAttributesFromLdap(LDAP_AUTODISCOVER_FILTER, ldapUrl, authorizationProperties, LDAP_AUTODISCOVER_ATTRIBUTE_VALUE, CONFIGURATION_BASE + COMMA + LDAP_BASE);
-            if (autodiscoverAttrMap != null && !autodiscoverAttrMap.isEmpty()) {
-                attrMap.putAll(autodiscoverAttrMap);
+                for (NamingEnumeration n = attributes.getAll(); n.hasMore(); ) {
+                    Attribute a = (Attribute) n.next();
+                    if (a.size() == 1) {
+                        List<String> lst = new ArrayList<String>();
+                        Collections.addAll(lst, a.get().toString());
+                        attrMap.put(a.getID(), lst);
+                    } else {
+                        ArrayList<String> resultAttributeValues = new ArrayList<String>();
+                        NamingEnumeration<?> attributeValues = a.getAll();
+                        while (attributeValues.hasMore()) {
+                            resultAttributeValues.add(attributeValues.next().toString());
+                        }
+                        attrMap.put(a.getID(), resultAttributeValues);
+                    }
+                }
             }
+        } catch (NamingException e) {
+            addMessage("Error while getting Root DSE: " + e.toString());
+        } finally {
+            return attrMap;
         }
-        return attrMap;
     }
 
     private Map<String, List<String>> getUserAttributesFromLdap(String searchFilter, String ldapUrl, Properties authorizationProperties, String[] returningAttributes, String ldapBase) {
+        Map<String, List<String>> attrMap = new HashMap<String, List<String>>();
         SearchControls searchControls = new SearchControls();
         searchControls.setReturningAttributes(returningAttributes);
         searchControls.setSearchScope(SearchControls.SUBTREE_SCOPE);
@@ -218,7 +262,8 @@ public class LdapChecker {
             if (answer.hasMore()) {
                 SearchResult sr = (SearchResult) answer.next();
                 Attributes attributes = sr.getAttributes();
-                Map<String, List<String>> attrMap = new HashMap<String, List<String>>();
+                addMessage("Get " + attributes.size() + " attributes from LDAP");
+
                 for (NamingEnumeration n = attributes.getAll(); n.hasMore(); ) {
                     Attribute a = (Attribute) n.next();
                     if (a.size() == 1) {
@@ -240,9 +285,14 @@ public class LdapChecker {
                 return null;
             }
         } catch (NamingException e) {
-            System.out.println("Error getting user attributes from ldap '" + ldapUrl + "':" + e.toString());
-            fileWriter.println("Error getting user attributes from ldap '" + ldapUrl + "':" + e.toString());
-            return null;
+            StringBuilder attributesBuilder = new StringBuilder();
+            for(String returningAttribute : returningAttributes) {
+                attributesBuilder.append(returningAttribute);
+                attributesBuilder.append(",");
+            }
+            String attributes = attributesBuilder.toString();
+            addMessage("Error getting user attributes:" + attributes + " from ldap '" + ldapUrl + "':" + e.toString());
+            return attrMap;
         } finally {
             try {
                 if (answer != null) {
@@ -286,6 +336,7 @@ public class LdapChecker {
 
         String autodiscoverAttribute = parameters.get(LDAP_AUTODISCOVER_ATTRIBUTE);
         if (autodiscoverAttribute != null && autodiscoverAttribute.length() > 0) {
+            this.AUTODISCOVER_BASE = parameters.get(LDAP_AUTODISCOVER_BASE);
             this.LDAP_AUTODISCOVER_ATTRIBUTE_VALUE = new String[]{autodiscoverAttribute};
         } else {
             this.LDAP_AUTODISCOVER_ATTRIBUTE_VALUE = null;
@@ -294,5 +345,10 @@ public class LdapChecker {
 
     private boolean useGlobalUrl() {
         return LDAP_GLOBAL_URL != null && LDAP_GLOBAL_URL.length() > 0;
+    }
+
+    private void addMessage(String message) {
+        System.out.println(message);
+        fileWriter.println(message);
     }
 }
